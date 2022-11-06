1eb8a6c5708ccbc73051a9fdad9df55d5fb105df
Onur Tas <adocresta@gmail.com>
2022-11-06 14:41:46 +0300

    date code ExpenseList

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,9 +3,27 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   const { expenses } = props;
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   return (
     <li className="expense-item">
-      <h2 className="expense-item__date">{expenses.date.toUTCString()}</h2>
+      <date className="expense-item__date">
+        <div>{month[expenses.date.getMonth()]}</div>
+        <div>{expenses.date.getDate()}</div>
+        <div>{expenses.date.getFullYear()}</div>
+      </date>
       <div className="expense-item__description">
         <h2>{expenses.title}</h2>
         <div className="expense-item__price">${expenses.amount}</div>
